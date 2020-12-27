671cf2851b90de0522d4918e5e6cb6a72521aefc
Asaf Meizner <AsafMeizner@gmail.com>
2020-12-27 14:18:33 +0200

    Start to work on screen

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 from drive_tools.straight_line import straight_line
 from drive_tools.turn import turn
 from drive_tools.follow_line import follow_line
+from pybricks.hubs import EV3Brick
 
 ev3 = EV3Brick()
+screen = ev3.screen
 right_motor = Motor(Port.C)
 left_motor =Motor(Port.D)
 right_mission_motor = Motor(Port.A)
@@ -19,3 +21,9 @@ gyro = GyroSensor(Port.S3, Direction.COUNTERCLOCKWISE)
 left_color_sensor = ColorSensor(Port.S4)
 right_color_sensor = ColorSensor(Port.S1)
 drive_base = DriveBase(left_motor, right_motor, 62.4, 15)
+
+screen.draw_image(0, 0, 'essential/images/run1.png')
+
+wait(100000)
+
+screen.clear()
