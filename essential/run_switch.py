a5c9e6be33b4d11efa924bf1e281f7a4aafb7875
Asaf Meizner <AsafMeizner@gmail.com>
2020-12-31 08:42:12 +0200

    Added fitchers to run switch

--- essential/run_switch.py
+++ essential/run_switch.py
@@ -6,6 +6,7 @@ from runs.run1 import run_1
 from pybricks.tools import wait
 from runs.run2 import run_2
 from runs.run3 import run_3
+from runs.run4 import run_4
 
 ev3 = EV3Brick()
 button = ev3.buttons
@@ -17,26 +18,29 @@ wait(10)
 
 def run_pick(left_mission_motor):
     run_number = 1
-    while True:
+    while not Button.RIGHT, Button.LEFT in button.pressed():
         screen.draw_image(0, 0, 'essential/images/run{}.png'.format(run_number))
         while not button.pressed():
             wait(20)
         if Button.DOWN in button.pressed():
             run_number = run_number + 1
-            wait(500)
+            wait(400)
         if Button.UP in button.pressed():
             run_number = run_number - 1
-            wait(500)
+            wait(400)
         if Button.CENTER in button.pressed():
             getattr(sys.modules[__name__], "run_{}".format(run_number))(left_mission_motor)
-            wait(500)
-        if run_number >=4:
+            wait(400)
+        if run_number >=5:
             run_number = 1
         if run_number <=0:
             run_number = 3
         if button.pressed():
             screen.clear()
         wait(10)
+         if Button.RIGHT, Button.LEFT in button.pressed():
+            screen.draw_image(0, 0, 'essential/images/2472end_screen.png')
+            wait(2000)
         
         
 
