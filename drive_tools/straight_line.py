1697f70f0e819be472d3f1089cce77271d2dfa0d
Asaf Meizner <AsafMeizner@gmail.com>
2021-02-24 19:12:19 +0200

    stepcounter misision done

--- drive_tools/straight_line.py
+++ drive_tools/straight_line.py
@@ -12,14 +12,13 @@ def p_controller(set_point, gyro):
 
 def straight_line(angle, speed, duration):
     timer = StopWatch()
-
+    
     while timer.time() <= duration: 
 
         fix_amount=p_controller(angle, gyro)
         drive_base.drive(speed, -fix_amount)   
     
 def straight_line_distance(angle, speed, distance):
-    gyro.reset_angle(0)
     drive_base.reset()
     while abs(drive_base.distance()) <= distance:
         fix_amount=p_controller(angle, gyro)
@@ -28,7 +27,6 @@ def straight_line_distance(angle, speed, distance):
     drive_base.stop()  
 
 def straight_line_light(angle, speed, color_sensor):
-    gyro.reset_angle(0)
     drive_base.reset()
     while color_sensor.reflection() >= 60:
         fix_amount=p_controller(angle, gyro)
--- runs/run2.py
+++ runs/run2.py
@@ -1,19 +1,21 @@
 from pybricks.ev3devices import (Motor)
-from essential.Object_creator import left_mission_motor
+from essential.Object_creator import left_mission_motor, right_mission_motor
 from essential.Object_creator import right_color_sensor, left_color_sensor
 from essential.Object_creator import right_motor, left_motor
-from drive_tools.follow_line import follow_line_right
 from drive_tools.straight_line import straight_line, straight_line_distance
-from drive_tools.turn import turn
-
+from drive_tools.follow_line import follow_line_left, follow_line_right
+from essential.Object_creator import gyro
 
 def run_2():
-    straight_line_distance(0, -100, 2000)
-    straight_line_distance(0, 200, 100)
-    turn(90, 0)
-    straight_line_distance(0, -100, 500)
-    turn(-90, 0)
-    follow_line_right(-200, 2000)
-     straight_line(0, 100, 4000)
-    turn(-50, 0)
-    straight_line_distance(0, -200, 200)
+    gyro.reset_angle(0)
+    straight_line_distance(-15, -100, 80)
+    straight_line_distance(-1, -150, 750)
+    straight_line(-5, -30, 9000)
+    straight_line_distance(45, 100, 120)
+    straight_line_distance(0, -150, 750)
+    # straight_line(0, -100, 1700)
+    # straight_line(-15, -100, 500)
+    # follow_line_right(-200, 400)
+    # straight_line(5, -200, 2300)
+    # straight_line(-5, -30, 20000)
+    # straight_line(-10, 200, 3000)
