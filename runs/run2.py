7cd0c5b58e5a7f82eb75e958f943560590dfd4ea
Asaf Meizner <AsafMeizner@gmail.com>
2021-04-07 15:23:57 +0300

    run 2 work

--- runs/run2.py
+++ runs/run2.py
@@ -5,17 +5,23 @@ from essential.Object_creator import right_motor, left_motor
 from drive_tools.straight_line import straight_line, straight_line_distance
 from drive_tools.follow_line import follow_line_left, follow_line_right
 from essential.Object_creator import gyro
+from pybricks.tools import wait
 
 def run_2():
     gyro.reset_angle(0)
     straight_line_distance(0, -100, 800)
-    left_mission_motor.run_time(-800, 2500)
-    # left_mission_motor.run_time(800, 2500)
-    straight_line_distance(-3, -25, 210)
+    wait(300)
+    straight_line_distance(-3, -25, 208)
     straight_line_distance(0, 100, 180)
-    straight_line_distance(-45, -100, 190)
+    straight_line_distance(-45, -100, 185)
     straight_line_distance(0, -100, 90)
     straight_line_distance(0, -100, 400)
-    straight_line_distance(0, -100, 260)
-    right_mission_motor.run_time(-800, 3200)
-    straight_line_distance(0, 200, 2000)
+    straight_line_distance(-4, -100, 200)
+    straight_line_distance(0, -100, 20)
+    right_mission_motor.run_time(-800, 5200)
+    straight_line_distance(0, 200, 570)
+    straight_line_distance(-40, 60, 100)
+    straight_line_distance(-40, 100, 130)
+    left_mission_motor.run_time(-800, 2500) 
+    straight_line_distance(-40, -100, 60)
+    straight_line_distance(0, 200, 1300)
