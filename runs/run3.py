11ed80bb50f836869aed83929bcdb8d3bb2004e2
Asaf Meizner <AsafMeizner@gmail.com>
2021-04-19 16:34:57 +0300

    run 1 works

--- runs/run3.py
+++ runs/run3.py
@@ -1,10 +1,15 @@
 from pybricks.ev3devices import (Motor)
-from essential.Object_creator import left_mission_motor
+from essential.Object_creator import left_mission_motor, gyro
 from essential.Object_creator import right_color_sensor, left_color_sensor
 from essential.Object_creator import right_motor, left_motor
 from pybricks.tools import wait
-from drive_tools.straight_line import straight_line, straight_line_distance
+from drive_tools.straight_line import straight_line, straight_line_distance, straight_line_light_black
+from drive_tools.turn import turn
 
 def run_3():
-  straight_line_distance(0, -20, 700)
-    
+  gyro.reset_angle(0)
+ # straight_line_light_black(0, -1000, right_color_sensor)
+  straight_line_distance(0, -100, 900)
+  turn(-90, 2)
+  straight_line_distance(-80, -100, 80)
+  straight_line_distance(-90, -100, 200)
