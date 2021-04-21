df7937f21938ad570136cd2a7ad39b01a7699c62
Asaf Meizner <AsafMeizner@gmail.com>
2021-04-21 15:20:05 +0300

    tast

--- runs/run4.py
+++ runs/run4.py
@@ -3,7 +3,7 @@ from essential.Object_creator import left_mission_motor
 from essential.Object_creator import right_color_sensor, left_color_sensor
 from essential.Object_creator import right_motor, left_motor
 from drive_tools.straight_line import straight_line
+from drive_tools.follow_line import follow_line_left, follow_line_right
 
 def run_4():
-    straight_line(0, -100, 1500)
-    straight_line(0, 100, 1500)
+    follow_line_right(-100, 1000)
